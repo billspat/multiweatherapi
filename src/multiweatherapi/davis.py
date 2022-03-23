9f3448e0d566b3f978005069bf63afcf6e23b06d
jhp <jhp@msu.edu>
2022-03-23 14:31:06 -0400

    davis station accepts 24hr+ data polling. Ref. #51

--- src/multiweatherapi/davis.py
+++ src/multiweatherapi/davis.py
@@ -2,7 +2,7 @@ import collections
 import hashlib
 import hmac
 import json
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from requests import Session, Request
 
 
@@ -17,14 +17,12 @@ class DavisParam:
         The customer's access key (v2)
     apisec : str
         API security that is used to compute the hash
-    start_date_org : datetime
-        Stores datetime object passed initially
     start_date : datetime (UTC expected)
         Return readings with timestamps ≥ start_time. Specify start_time in str. (2021-08-01, 2021-08-01)
-    end_date_org : datetime
-        Stores datetime object passed initially
     end_date : datetime (UTC expected)
         Return readings with timestamps ≤ end_time. Specify end_time in str. (2021-08-31, 2021-08-31)
+    date_tuple_list : list
+        A list of date/time tuples with API signatures
     conversion_msg : str
         Stores time conversion message
     json_file : str, optional
@@ -37,14 +35,13 @@ class DavisParam:
         self.sn = sn
         self.apikey = apikey
         self.apisec = apisec
-        self.apisig = None
+        self.apisig = None  # used when start_date & end_date is passed
         self.t = int(datetime.now().timestamp())
         # self.start_date = int(time.mktime(time.strptime(start_date, "%m/%d/%Y %H:%M"))) if start_date else None
         # self.end_date = int(time.mktime(time.strptime(end_date, "%m/%d/%Y %H:%M"))) if end_date else None
-        self.start_date_org = start_date
         self.start_date = start_date
-        self.end_date_org = end_date
         self.end_date = end_date
+        self.date_tuple_list = list()
         self.conversion_msg = ''
         self.json_file = json_file
         self.binding_ver = binding_ver
@@ -67,53 +64,84 @@ class DavisParam:
         if self.sn is None:
             raise Exception('"sn" parameter must be included.')
 
+        if self.start_date and self.end_date:
+            if self.end_date - timedelta(hours=24) < self.start_date:
+                self.date_tuple_list.append((self.start_date, self.end_date))
+            else:
+                while self.end_date - timedelta(hours=24) >= self.start_date:
+                    self.date_tuple_list.append(
+                        (self.start_date, self.start_date + timedelta(hours=23, minutes=59, seconds=59)))
+                    self.start_date += timedelta(hours=24)
+                self.date_tuple_list.append((self.start_date, self.end_date))
+            for elem in self.date_tuple_list:
+                print(elem)
+
     def __utc_to_local(self):
+        # this method does not affect the API outcome at all may be removed without any issue
         print('UTC Start date: {}'.format(self.start_date))
-        self.conversion_msg += 'UTC start date passed as parameter: {}'.format(self.start_date) + " \\ "
+        # self.conversion_msg += 'UTC start date passed as parameter: {}'.format(self.start_date) + " \\ "
         self.conversion_msg += 'Davis utilizes Unix Epoch, just added explicit UTC timezone' + " \\ "
         # self.start_date = self.start_date.replace(tzinfo=timezone.utc).astimezone(tz=None)
         # if self.start_date else None
         self.start_date = self.start_date.replace(tzinfo=timezone.utc) if self.start_date else None
         print('Explicit UTC time Start date: {}'.format(self.start_date))
         self.conversion_msg += 'Explicit UTC time start date after conversion: {}'.format(self.start_date) + " \\ "
-
+        #
         print('UTC End date: {}'.format(self.end_date))
         self.conversion_msg += 'UTC end date passed as parameter: {}'.format(self.end_date) + " \\ "
-        # self.end_date = self.end_date.replace(tzinfo=timezone.utc).astimezone(tz=None) if self.end_date else None
+        # # self.end_date = self.end_date.replace(tzinfo=timezone.utc).astimezone(tz=None) if self.end_date else None
         self.end_date = self.end_date.replace(tzinfo=timezone.utc) if self.end_date else None
         self.conversion_msg += 'Explicit UTC time end date after conversion: {}'.format(self.end_date) + " \\ "
         print('Explicit UTC time End date: {}'.format(self.end_date))
 
     def __format_time(self):
         self.__utc_to_local()
-        self.start_date = int(self.start_date.timestamp()) if self.start_date else None
-        self.end_date = int(self.end_date.timestamp()) if self.end_date else None
+        # self.start_date = int(self.start_date.timestamp()) if self.start_date else None
+        # self.end_date = int(self.end_date.timestamp()) if self.end_date else None
+        if self.start_date and self.end_date:
+            temp_list = list()
+            for st, ed in self.date_tuple_list:
+                st = int(st.replace(tzinfo=timezone.utc).timestamp())
+                ed = int(ed.replace(tzinfo=timezone.utc).timestamp())
+                temp_list.append((st, ed))
+            self.date_tuple_list = temp_list
 
     def __compute_signature(self):
-        params = {
-            'api-key': self.apikey,
-            'station-id': self.sn,
-            't': self.t
-        }
+
+        def compute_signature_engine():  # compute_engine
+            for key in params:
+                print("Parameter name: \"{}\" has value \"{}\"".format(key, params[key]))
+
+            data = ""
+            for key in params:
+                data = data + key + str(params[key])
+            print("Data string to hash is: \"{}\"".format(data))
+
+            sig = hmac.new(
+                self.apisec.encode('utf-8'),
+                data.encode('utf-8'),
+                hashlib.sha256).hexdigest()
+            print("API Signature is: \"{}\"".format(sig))
+            return sig
+
         if self.start_date and self.end_date:
-            params['start-timestamp'] = self.start_date
-            params['end-timestamp'] = self.end_date
-
-        params = collections.OrderedDict(sorted(params.items()))
-        for key in params:
-            print("Parameter name: \"{}\" has value \"{}\"".format(key, params[key]))
-
-        data = ""
-        for key in params:
-            data = data + key + str(params[key])
-        print("Data string to hash is: \"{}\"".format(data))
-
-        self.apisig = hmac.new(
-            self.apisec.encode('utf-8'),
-            data.encode('utf-8'),
-            hashlib.sha256
-        ).hexdigest()
-        print("API Signature is: \"{}\"".format(self.apisig))
+            temp_list = list()
+            for st, ed in self.date_tuple_list:
+                params = {'api-key': self.apikey,
+                          'station-id': self.sn,
+                          't': self.t,
+                          'start-timestamp': st,
+                          'end-timestamp': ed}
+                params = collections.OrderedDict(sorted(params.items()))
+                apisig = compute_signature_engine()
+                temp_list.append((st, ed, apisig))
+            self.date_tuple_list = temp_list
+            for elem in self.date_tuple_list:
+                print(elem)
+        else:
+            params = {'api-key': self.apikey, 'station-id': self.sn, 't': self.t}
+            params = collections.OrderedDict(sorted(params.items()))
+            self.apisig = compute_signature_engine()
 
 
 class DavisReadings:
@@ -121,10 +149,10 @@ class DavisReadings:
     A class used to represent a device's readings
     Attributes
     ----------
-    request : Request
-        a Request object defining the request made to the Davis server
-    response : Response
-        a json response from the Davis server
+    requests : list
+        a list of Request objects defining the request made to the Davis server
+    responses : list
+        a (combined) raw json responses from the Davis server
     parsed_resp : list of dict
         a parsed response from
     debug_info : dict
@@ -141,27 +169,27 @@ class DavisReadings:
         self.debug_info = {
             'sn': param.sn,
             'apikey': param.apikey,
-            'apisig': param.apisig,
             't': param.t,
-            'start_date_org': param.start_date_org,
-            'start_date': param.start_date,
-            'end_date_org': param.end_date_org,
-            'end_date': param.end_date,
+            'apisig': param.apisig,
+            'date_tuple_list': param.date_tuple_list,
             'conversion_msg': param.conversion_msg,
             'json_str': param.json_file,
             'binding_ver': param.binding_ver
         }
+        self.requests = list()
+        self.responses = list()
+
         if param.json_file:
             self.response = json.load(open(param.json_file))
             self.__parse()
         elif param.sn and param.apikey:
-            self.__get(param.sn, param.apikey, param.apisig, param.t, param.start_date, param.end_date)
+            self.__get(param.sn, param.apikey, param.apisig, param.date_tuple_list, param.t)
         elif param.sn or param.apikey:
             raise Exception('"sn" and "apikey" parameters must both be included.')
         else:
             # build an empty DavisToken
-            self.request = None
-            self.response = None
+            self.requests = None
+            self.responses = None
             self.parsed_resp = None
             # self.device_info = None
             # self.measurement_settings = None
@@ -169,7 +197,7 @@ class DavisReadings:
             # self.locations = None
             # self.installation_metadata = None
 
-    def __get(self, sn, apikey, apisig, t, start_date=None, end_date=None):
+    def __get(self, sn, apikey, apisig, date_tuple_list, t):
         """
         Gets a device readings using a GET request to the Davis API.
         Wraps build and parse functions.
@@ -181,19 +209,17 @@ class DavisReadings:
             The user's API access key
         apisig : str
             API signature calculated when parameter object is instantiated
+        date_tuple_list : list
+            A list of date/time tuples with API signatures
         t : int
             Unix timestamp when the query is submitted
-        start_date : int, optional
-            Return readings with timestamps ≥ start_date.
-        end_date : int, optional
-            Return readings with timestamps ≤ end_date.
         """
-        self.__build(sn, apikey, apisig, t, start_date, end_date)
+        self.__build(sn, apikey, apisig, date_tuple_list, t)
         self.__make_request()
         self.__parse()
         return self
 
-    def __build(self, sn, apikey, apisig, t, start_date=None, end_date=None):
+    def __build(self, sn, apikey, apisig, date_tuple_list, t):
         """
         Gets a device readings using a GET request to the Davis API.
         Parameters
@@ -204,31 +230,31 @@ class DavisReadings:
             The user's API access key
         apisig : str
             API signature calculated when parameter object is instantiated
+        date_tuple_list : list
+            A list of date/time tuples with API signatures
         t : int
             Unix timestamp when the query is submitted
-        start_date : int, optional
-            Return readings with timestamps ≥ start_date.
-        end_date : int, optional
-            Return readings with timestamps ≤ end_date.
         """
-        if start_date and end_date:
-            self.request = Request('GET',
-                                   url='https://api.weatherlink.com/v2/historic/' + sn,
-                                   params={'api-key': apikey,
-                                           't': t,
-                                           'start-timestamp': start_date,
-                                           'end-timestamp': end_date,
-                                           'api-signature': apisig}).prepare()
+        if len(date_tuple_list) != 0:
+            for st, ed, sig in date_tuple_list:
+                tmp_request = Request('GET',
+                                      url='https://api.weatherlink.com/v2/historic/' + sn,
+                                      params={'api-key': apikey,
+                                              't': t,
+                                              'start-timestamp': st,
+                                              'end-timestamp': ed,
+                                              'api-signature': sig}).prepare()
+                self.requests.append(tmp_request)
         else:
-            self.request = Request('GET',
-                                   url='https://api.weatherlink.com/v2/current/' + sn,
-                                   params={'api-key': apikey,
-                                           't': t,
-                                           'api-signature': apisig}).prepare()
+            tmp_request = Request('GET',
+                                  url='https://api.weatherlink.com/v2/current/' + sn,
+                                  params={'api-key': apikey, 't': t, 'api-signature': apisig}).prepare()
+            self.requests.append(tmp_request)
 
-        self.debug_info['http_method'] = self.request.method
-        self.debug_info['url'] = self.request.url
-        self.debug_info['headers'] = self.request.headers
+        self.debug_info['requests'] = self.requests
+        # self.debug_info['http_method'] = self.request.method
+        # self.debug_info['url'] = self.request.url
+        # self.debug_info['headers'] = self.request.headers
         return self
 
     def __make_request(self):
@@ -236,20 +262,22 @@ class DavisReadings:
         Sends a token request to the Davis API and stores the response.
         """
         # Send the request and get the JSON response
-        resp = Session().send(self.request)
-        if resp.status_code != 200:
-            raise Exception(
-                'Request failed with \'{}\' status code and \'{}\' message.'.format(resp.status_code, resp.text))
-        self.response = resp.json()
-        self.debug_info['response'] = self.response
-        self.response['python_binding_version'] = self.debug_info['binding_ver']
+        for req in self.requests:
+            resp = Session().send(req)
+            if resp.status_code != 200:
+                raise Exception(
+                    'Request failed with \'{}\' status code and \'{}\' message.'.format(resp.status_code, resp.text))
+            self.responses.append(resp.json())
+            # self.debug_info['response'] = self.response
+
+        self.responses.append({'python_binding_version': self.debug_info['binding_ver']})
         return self
 
     def __parse(self):
         """
         Parses the response.
         """
-        self.parsed_resp = []
+        self.parsed_resp = list()
         # try:
         #     self.device_info = self.response['device']['device_info']
         # except KeyError:
